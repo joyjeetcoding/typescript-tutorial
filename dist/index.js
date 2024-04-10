9e25cc8945f69f6a294afba1b5d2bbefbc412883
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-04-10 17:07:30 +0530

    Added some files

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === "number") {
-        return weight * 2.2;
-    }
-    else {
-        return parseInt(weight) * 2.2;
-    }
+// let age: number = 20;
+var _a;
+function getcustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
-kgToLbs(10);
-kgToLbs('10kg');
+let customer = getcustomer(1);
+// Optional Chaining
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+// Optional Element Access Operator
+// customer?.[0]
+// Optional Call
+let log = null;
+log === null || log === void 0 ? void 0 : log('a');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,76 @@
 // let age: number = 20;
 
-// if(age < 50)
-//     age += 10
+// if (age < 50) age += 10;
 
 // console.log(age);
 
 // let sales = 123456789;
 
-// let course = 'TypeScript';
+// let course = "TypeScript";
 
 // let is_published = true;
 
 // let level;
 
 // level = 1;
-// level = "0"
+// level = "0";
 
-// function render(document) {
-//     console.log(document);
+// // function render(document) {
+// //     console.log(document);
 
-// }
+// // }
 
-// Arrays
+// // Arrays
 
-// let numbers: string[] = []
+// let numbers: string[] = [];
 
-// numbers.forEach(n => n.)
+// // numbers.forEach(n => n.)
 
-// numbers[0] = 1;
-// numbers[1] = 2;
+// // numbers[0] = 1;
+// // numbers[1] = 2;
 
 // console.log(numbers[1]);
 
-// Tuples
+// // Tuples
 
-// let user: [number, string] = [1, 'Joyjeet'];
-// user.push(1)
+// let user: [number, string] = [1, "Joyjeet"];
+// user.push(1);
 
-// Enums
+// // Enums
 
 // const small = 1;
 // const medium = 2;
 // const large = 3;
 
 // // PascalCase
-// const enum Size { Small = 1, Medium, Large }
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
 
-// let mySize: Size = Size.Medium
+// let mySize: Size = Size.Medium;
 
 // console.log(mySize);
 
-// Functions
+// // Functions
 
 // function calculateTax(income: number, taxYear = 2020): number {
-//     if((taxYear) < 2022) {
-//         return income * 1.2
-//     }
-//     return income * 1.3
+//   if (taxYear < 2022) {
+//     return income * 1.2;
+//   }
+//   return income * 1.3;
 // }
 
 // calculateTax(10_000);
 
-// Objects
+// // Objects
 
 // type Employee = {
-//     readonly id: number;
-//     name: string;
-//     retire: (date: Date) => void;
-// }
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// };
 
 // let employee: Employee = {
 //   id: 1,
@@ -76,23 +79,77 @@
 //     console.log(date);
 //   },
 // };
-// employee.name = "Joy"       //Valid in JS.. but not in TS
+// employee.name = "Joy"; //Valid in JS.. but not in TS
 
-// employee.id = 0
+// // employee.id = 0
 
+// // Union Types
 
-// Union Types
+// function kgToLbs(weight: number | string): number {
+//   // Narrowing
 
-function kgToLbs(weight: number | string): number {
-  // Narrowing
+//   if (typeof weight === "number") {
+//     return weight * 2.2;
+//   } else {
+//     return parseInt(weight) * 2.2;
+//   }
+// }
 
-  if(typeof weight === "number") {
-    return weight * 2.2
-  } else {
-    return parseInt(weight) * 2.2
-  }
+// kgToLbs(10);
+// kgToLbs("10kg");
 
+// // Intersection Type
+
+// type Draggable = {
+//   drag: () => void;
+// };
+
+// type Resizable = {
+//   resize: () => void;
+// };
+
+// type UIWidget = Draggable & Resizable; //Intersection
+
+// let textBox: UIWidget = {
+//   drag: () => {},
+//   resize: () => {},
+// };
+
+// // Literal Types
+// // literal means exact, specific type
+// type Quantity = 50 | 100;
+
+// let quantity: Quantity = 100;
+
+// type Metric = "cm" | "inch";
+
+// // Nullable Types
+// function greet(name: string | null | undefined) {
+//   if (name) console.log(name.toUpperCase());
+//   else console.log("Hola!");
+// }
+
+// greet(null);
+
+// Optional Chaining
+
+type Customer = {
+  birthday?: Date;
+};
+
+function getcustomer(id: number): Customer | null | undefined{
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-kgToLbs(10)
-kgToLbs('10kg')
+let customer = getcustomer(1)
+
+// Optional Chaining
+console.log(customer?.birthday?.getFullYear());
+
+// Optional Element Access Operator
+// customer?.[0]
+
+
+// Optional Call
+let log: any = null;
+log?.('a')
